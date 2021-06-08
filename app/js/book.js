c827995a4d5b96aa0cc4d2189f3de8bf809b274f
Dan Menjivar <41385232+danmenjivar@users.noreply.github.com>
2021-06-08 14:08:48 -0700

    Add manually displaying books to webpage

--- app/js/book.js
+++ app/js/book.js
@@ -1,3 +1,14 @@
+// TODO
+// - style cards
+//  - make so each one gets a unique color
+//  - style fonts (size & family)
+// * Add New Book button
+// * Add a button to each book to remove it from library
+// * Add a read status switch to each book
+// * Integrate local storage
+// * Integrate firebase
+
+
 let myLibrary = []; // stores books
 
 // Book Object Constructor
@@ -17,29 +28,46 @@ class Book {
     toggleReadStatus() {
         this.read = !this.read;
     }
-
-
-
 }
-// TODO
-// - Code a CSS card
-//  - write displayBooks()
-//      - loops through array and displays each book in a table, each in card
-// X Manually create some books & add them to the array
+
 
 function addBookToLibrary(title, author, pagesCount, readCondition) {
     myLibrary.push(new Book(title, author, pagesCount, readCondition));
 }
 
 function displayBooks() {
+    let libraryContainer = document.querySelector(".library-container");
     myLibrary.forEach(book => {
-        console.log({ book });
+        let card = document.createElement("div");
+        card.classList.add("card");
 
+        let title = document.createElement("h3");
+        title.textContent = book.title;
+        card.appendChild(title);
+
+        let author = document.createElement("p");
+        author.textContent = `by: ${book.author}`;
+        card.appendChild(author);
+
+        let pages = document.createElement("p");
+        pages.textContent = `${book.pages} pages`
+        card.appendChild(pages)
+
+        let read = document.createElement("p");
+        read.textContent = (book.read) ? "Read" : "Not Read";
+        card.appendChild(read);
+
+        libraryContainer.appendChild(card);
     });
 }
 
+// temp hard core demo really
+function demo() {
+    addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
+    addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+    addBookToLibrary("Don Quixote", "Miguel de Cervantes", 1072, false);
+    addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1178, false);
+    displayBooks();
+}
 
-addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true)
-addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true)
-addBookToLibrary("Don Quixote", "Miguel de Cervantes", 1072, false)
-addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1178, false)
+demo();
